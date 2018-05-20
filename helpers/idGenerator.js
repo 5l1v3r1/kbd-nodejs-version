a66095883e1fd9b889da8a7852ffde248eaaf741
SadeghHayeri <hayerisadegh@gmail.com>
2018-05-20 12:51:06 +0430

    use 'Uniqid' package to genarate random ID!

--- helpers/idGenerator.js
+++ helpers/idGenerator.js
@@ -1,6 +1,5 @@
-module.exports = function () {
-    // Math.random should be unique because of its seeding algorithm.
-    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-    // after the decimal.
-    return '_' + Math.random().toString(36).substr(2, 9);
+let uniqid = require('uniqid');
+
+module.exports = () => {
+    return uniqid();
 };
